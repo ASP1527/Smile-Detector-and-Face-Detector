71eb6889ca53a82c474d40f33ea14b3da58ca9a7
Amrit Singh Panesar <66624024+ASP1527@users.noreply.github.com>
2020-08-18 12:02:47 +0100

    modified:   SmileDetector.py

--- SmileDetector.py
+++ SmileDetector.py
@@ -26,6 +26,7 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 200, 50), 4)
 
+        # crop the face so it detects smiles within a face uses numpy N-dimensional slicing
         the_face = frame[y:y+h, x:x+w]
         # changes the face to grayscale
         face_greyscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
@@ -35,9 +36,11 @@ while True:
             face_greyscale, scaleFactor=1.7, minNeighbors=20)
 
         # draws a box around each smile, rgb is invertes in this module/ runs smile detection for the smile in the face
-        for (x_, y_, w_, h_) in smiles:
-            cv2.rectangle(frame, (x_, y_), (x_ + w_, y_ + h_),
-                          (50, 50, 200), 4)
+        # for (x_, y_, w_, h_) in smiles:
+        #cv2.rectangle(the_face, (x_, y_), (x_ + w_, y_ + h_),(50, 50, 200), 4)
+        if len(smiles) > 0:
+            cv2.putText(frame, 'Smiling', (x, y+h+40), fontScale=3,
+                        fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255))
 
     # shows the image, imshow=image show, shows the caption and the frame
     cv2.imshow('Smile Detector', frame)
